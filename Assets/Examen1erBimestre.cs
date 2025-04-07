05d01af9ec126c776b53b8b4c7b035265fdc5f30
JonasLeiserson <49516747@est.ort.edu.ar>
2025-04-07 10:25:24 -0300

    GUIA PREUBA 1 CONVERSION

---
+++ Assets/Examen1erBimestre.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Examen1erBimestre : MonoBehaviour
+{
+    public float MontoAConvertir;
+    public string DivisaALaQueConvetir;
+    float MontoFinal;
+    float montoMinimoAConvertir = 1000;
+    float CotizacionDolar = 1300f;
+    float CotizacionEuro = 1500f;
+    float CotizacionReal = 200f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (DivisaALaQueConvetir == "D" && MontoAConvertir > montoMinimoAConvertir || DivisaALaQueConvetir == "R" && MontoAConvertir > montoMinimoAConvertir || DivisaALaQueConvetir == "E" && MontoAConvertir > montoMinimoAConvertir)
+        {
+            switch(DivisaALaQueConvetir)
+            {
+                case("D"):
+                    
+                    MontoFinal = MontoAConvertir / CotizacionDolar;
+                    Debug.Log("La conversion de " + MontoAConvertir + " a  Dolar es " + MontoFinal + " Dolares");
+                    break;
+
+                case ("R"):
+                   
+                    MontoFinal = MontoAConvertir / CotizacionReal;
+                    Debug.Log("La conversion de " + MontoAConvertir + " a Real es" + MontoFinal + " Reales");
+                    break;
+
+                case ("E"):
+                    
+                    MontoFinal = MontoAConvertir / CotizacionEuro;
+                    Debug.Log("La conversion de " + MontoAConvertir + " a  Euro es" + MontoFinal + " Euros");
+                    break;
+
+            }
+        }
+        else if (MontoAConvertir < montoMinimoAConvertir)
+        {
+            Debug.Log("El monto mínimo es $1000");
+            return;
+        }
+              
+        else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+            return;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
